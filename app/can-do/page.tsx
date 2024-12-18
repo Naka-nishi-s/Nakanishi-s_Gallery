988fd5e0415abd31675939bff0e2bbc7a5df3ae0
Hyodo <90432430+Naka-nishi-s@users.noreply.github.com>
2024-12-19 02:35:30 +0900

    Topページ完成、各ページ配置完了

---
+++ app/can-do/page.tsx
@@ -0,0 +1,3 @@
+export default function CanDo() {
+  return <div>Can Do</div>;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 
 import dynamic from "next/dynamic";
 import { Caveat } from "next/font/google";
+import { CSSProperties } from "react";
 import animationJson from "../public/data.json";
 import styles from "./page.module.css";
 
@@ -14,15 +15,30 @@ const Caveat400 = Caveat({
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div>
+      <div className={`${styles.title} ${styles.fadeIn}`}>
         <h1 className={Caveat400.className}>{"Nakanishi's"}</h1>
         <h1 className={Caveat400.className}>Gallery</h1>
       </div>
-      <div>
-        <a href="/about-me">About me</a>
-        <a href="">Achievement</a>
+
+      <div className={styles.balloons}>
+        <div
+          style={{ "--delay": "1.6s" } as CSSProperties}
+          className={`${styles.balloons__balloon} ${styles.fall}`}
+        >
+          <a href="/about-me">About me</a>
+        </div>
+        <div
+          style={{ "--delay": "2s" } as CSSProperties}
+          className={`${styles.balloons__balloon} ${styles.fall}`}
+        >
+          <a href="#">Achievement</a>
+        </div>
       </div>
-      <div style={{ width: "60%", height: "auto" }}>
+
+      <div
+        style={{ "--delay": "0.6s" } as CSSProperties}
+        className={`${styles.work} ${styles.fadeIn}`}
+      >
         <Lottie animationData={animationJson} />
       </div>
     </main>
